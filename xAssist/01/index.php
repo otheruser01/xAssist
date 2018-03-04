7603f01ad062bd423fbb3146dde37f8dd917791b
afujiu <afujiu@DESKTOP-D8LPCTU>
2018-03-05 08:00:51 +0900

    レコードwav化

--- xAssist/01/index.php
+++ xAssist/01/index.php
@@ -9,109 +9,68 @@ $title="01.Speaker Recognition APIテスト";?>
 		<meta http-equiv=”Pragma” content=”no-cache”>
 		<meta http-equiv=”Cache-Control” content=”no-cache”>
 		<script src="js/common/lib/jquery-3.2.1.min.js"></script>
+		<script src="js/common/lib/recorder.js"></script>
 		<script>
-		var localMediaStream = null;
-        var localScriptProcessor = null;
-        audioData = []; // 録音データ
-        var bufferSize = 4096;
-        var onAudioProcess = function(e) {
-          var input = e.inputBuffer.getChannelData(0);
-          var bufferData = new Float32Array(bufferSize);
-          for (var i = 0; i < bufferSize; i++) {
-            bufferData[i] = input[i];
-          }
-          audioData.push(bufferData);
-        };
-        var startRecording = function() {
-        	var audioContext=new AudioContext();
-          navigator.getUserMedia(
-            { audio: true },
-            function(stream) {
-              localMediaStream = stream;
-              var scriptProcessor = audioContext.createScriptProcessor(bufferSize, 1, 1);
-              localScriptProcessor = scriptProcessor;
-              var mediastreamsource = audioContext.createMediaStreamSource(stream);
-              mediastreamsource.connect(scriptProcessor);
-              scriptProcessor.onaudioprocess = onAudioProcess;
-              scriptProcessor.connect(audioContext.destination);
-            },
-            function(e) {
-              console.log(e);
-            }
-          );
-          return audioContext;
-        };
-
-        var exportWAV = function(audioContext,audioData) {
-            var encodeWAV = function(samples, sampleRate) {
-              var buffer = new ArrayBuffer(44 + samples.length * 2);
-              var view = new DataView(buffer);
-
-              var writeString = function(view, offset, string) {
-                for (var i = 0; i < string.length; i++){
-                  view.setUint8(offset + i, string.charCodeAt(i));
-                }
-              };
-
-              var floatTo16BitPCM = function(output, offset, input) {
-                for (var i = 0; i < input.length; i++, offset += 2){
-                  var s = Math.max(-1, Math.min(1, input[i]));
-                  output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
-                }
-              };
-
-              writeString(view, 0, 'RIFF');  // RIFFヘッダ
-              view.setUint32(4, 32 + samples.length * 2, true); // これ以降のファイルサイズ
-              writeString(view, 8, 'WAVE'); // WAVEヘッダ
-              writeString(view, 12, 'fmt '); // fmtチャンク
-              view.setUint32(16, 16, true); // fmtチャンクのバイト数
-              view.setUint16(20, 1, true); // フォーマットID
-              view.setUint16(22, 1, true); // チャンネル数
-              view.setUint32(24, sampleRate, true); // サンプリングレート
-              view.setUint32(28, sampleRate * 2, true); // データ速度
-              view.setUint16(32, 2, true); // ブロックサイズ
-              view.setUint16(34, 16, true); // サンプルあたりのビット数
-              writeString(view, 36, 'data'); // dataチャンク
-              view.setUint32(40, samples.length * 2, true); // 波形データのバイト数
-              floatTo16BitPCM(view, 44, samples); // 波形データ
-
-              return view;
-            };
-
-            var mergeBuffers = function(audioData) {
-              var sampleLength = 0;
-              for (var i = 0; i < audioData.length; i++) {
-                sampleLength += audioData[i].length;
-              }
-              var samples = new Float32Array(sampleLength);
-              var sampleIdx = 0;
-              for (var i = 0; i < audioData.length; i++) {
-                for (var j = 0; j < audioData[i].length; j++) {
-                  samples[sampleIdx] = audioData[i][j];
-                  sampleIdx++;
-                }
-              }
-              return samples;
-            };
-
-            var dataview = encodeWAV(mergeBuffers(audioData), audioContext.sampleRate);
-            var audioBlob = new Blob([dataview], { type: 'audio/wav' });
-
-            var myURL = window.URL || window.webkitURL;
-            var url = myURL.createObjectURL(audioBlob);
-            return url;
-          };
-		var  audioContext= startRecording();
         $(function(){
-	        $("#export").on('click', function() {
-	        	console.log(exportWAV(audioContext,audioData));
-	        });
+			$("#record").click(function(){
+				startRecording();
+				});
+			$("#export").click(function(){
+				stopRecording();
+				});
+
         });
+		  var audio_context;
+		  var recorder;
+
+
+  	  function startUserMedia(stream) {
+		    var input = audio_context.createMediaStreamSource(stream);
+		    recorder = new Recorder(input);
+		  }
+
+		  function startRecording() {
+		    recorder && recorder.record();
+		  }
+
+		  function stopRecording() {
+		    recorder && recorder.stop();
+		    createDownloadLink();
+		    recorder.clear();
+		  }
+
+		  function createDownloadLink() {
+		    recorder && recorder.exportWAV(function(blob) {
+		      var url = URL.createObjectURL(blob);
+		      var au = document.getElementById('audio');
+
+		      au.controls = true;
+		      au.src = url;
+		      hf.href = url;
+		      hf.download = new Date().toISOString() + '.wav';
+		    });
+		  }
+		  window.onload = function init() {
+			    try {
+			      window.AudioContext = window.AudioContext || window.webkitAudioContext;
+			      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+			      window.URL = window.URL || window.webkitURL;
+			      audio_context = new AudioContext;
+			    } catch (e) {
+			      alert('No web audio support in this browser!');
+			    }
+
+			    navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
+			      __log('No live audio input: ' + e);
+			    });
+			  }
 	</script>
 	</head>
 	<body>
 		<h1><?php echo $title;?></h1>
 		音声を録音してSpeakerRecogntionに保存。
+		<button id="record">録音</button>
 		<button id="export">出力</button>
+		<audio id="audio" controls></audio>
 	</body>
 </html>
